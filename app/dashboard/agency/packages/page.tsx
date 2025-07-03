d85863339828b7564f2f466c7840dada04f946f5
Toyyib <sannianibiretoyyib@yahoo.com>
2025-07-03 07:29:48 +0100

    added use client to the agency package page

--- app/dashboard/agency/packages/page.tsx
+++ app/dashboard/agency/packages/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react"
 import ProtectedRoute from "@/components/protected-route"
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
