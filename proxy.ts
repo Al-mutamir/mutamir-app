14441e76661e00e6862d25fcdb6b9a7ee896260e
unknown <sannianibiretoyyib@yahoo.com>
2026-07-29 07:45:55 -0700

    changed middleware-to-proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 // Define the middleware function
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Get the pathname from the request
   const path = request.nextUrl.pathname
 
